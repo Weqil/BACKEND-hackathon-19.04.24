861d30db20c80f1b639a72813913fa6bdfb8a4fc
Ada-lave <113159483+Ada-lave@users.noreply.github.com>
2024-04-20 22:46:54 +0500

    make a email notification

--- app/Console/Commands/FoundACouple.php
+++ app/Console/Commands/FoundACouple.php
@@ -2,10 +2,15 @@
 
 namespace App\Console\Commands;
 
+use App\Mail\NotificationAboutCouple;
+use App\Mail\NotificationAboutCoupleNotFound;
 use App\Models\Company;
 use App\Models\Meeting;
 use App\Models\User;
+use Carbon\Carbon;
+use Exception;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Mail;
 
 class FoundACouple extends Command
 {
@@ -32,6 +37,7 @@ class FoundACouple extends Command
         $companyWithMeetingInThisDayIds = $this->getCompaniesByMeetingWeekDay($weekDay)->pluck("id");
 
         foreach($companyWithMeetingInThisDayIds as $company_id){
+            $this->spendOldNotAcceptedMeetings($company_id);
             $users = $this->getCompanyUsers($company_id)->toArray();
             shuffle($users);
             $splitedUsers = $this->splitUsers($users);
@@ -51,15 +57,44 @@ class FoundACouple extends Command
                 $meeting = Meeting::create([
                     "title" => "Встреча ".$coupleUsers[0]["name"]." с ".$coupleUsers[1]["name"],
                     "company_id" => $company_id
-                ]);   
+                ]);
                 $meeting->users()->attach([$coupleUsers[0]["id"],$coupleUsers[1]["id"]]);
+                // dd($coupleUsers[0]["email"]);
+
+                try{
+                    Mail::to($coupleUsers[0]['email'])->send(new NotificationAboutCouple());
+                    Mail::to($coupleUsers[1]['email'])->send(new NotificationAboutCouple());
+                }
+                catch (Exception $e){
+
+                }
+
             }
             else{
-                echo "sorry ".$coupleUsers[0]["name"];
+                Mail::to($coupleUsers[0]['email'])->send(new NotificationAboutCoupleNotFound());
             }
         }
     }
 
+    public function spendOldNotAcceptedMeetings($company_id){
+        $meetings = Company::find($company_id)->meetings;
+
+        foreach($meetings as $meeting){
+            $diffDays = $this->getDaysDifferent($meeting["created_at"]);
+            if($meeting["status"] == null && $diffDays < -3){
+                $meeting->update([
+                    "status" => false
+                ]);
+            }
+        }
+    }
+
+    private function getDaysDifferent($meetingCreatedDate){
+        $meetingDate = Carbon::parse($meetingCreatedDate);
+        $today = Carbon::today();
+
+        return $today->diffInDays($meetingDate);
+    }
     private function splitUsers($users){
         return array_chunk($users, 2);
     }
---
+++ app/Mail/NotificationAboutCoupleNotFound.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class NotificationAboutCoupleNotFound extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: 'Random Coffee к сожелению для вас не удалост найти напарника',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+        return new Content(
+            view: 'mail.coupleNotFound',
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array
+    {
+        return [];
+    }
+}
